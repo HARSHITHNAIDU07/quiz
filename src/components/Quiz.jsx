4daff269374035e619e8aa16a6a4bf2d46848cb3
EARLE HARSHITH NAIDU <322103310062@gvpce.ac.in>
2025-06-20 15:58:59 +0530

    first commit

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,38 @@
+import '../quiz.css';
+const Questions=[
+    {
+        question: "What is the capital of France?",
+        options: ["Berlin", "Madrid", "Paris", "Rome"],
+        answer: "Paris"
+    },
+    {
+        question: "What is 2 + 2?",
+        options: ["3", "4", "5", "6"],
+        answer: "4"
+    },
+    {
+        question: "What is the largest planet in our solar system?",
+        options: ["Earth", "Mars", "Jupiter", "Saturn"],
+        answer: "Jupiter"
+    },
+    {
+        question: "Who wrote 'To Kill a Mockingbird'?",
+        options: ["Harper Lee", "Mark Twain", "Ernest Hemingway", "F. Scott Fitzgerald"],
+        answer: "Harper Lee"
+    }
+]
+
+
+
+function Quiz(){
+    return(
+        <>
+        <div className='box'>
+            
+        </div>
+        </>
+    )
+
+    
+};
+export default Quiz;
